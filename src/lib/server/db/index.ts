9fbd743c041d1d875e06303de14eb48ea5ea12a3
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-02 16:03:27 +0900

    fixing cicd

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 import { env } from "$env/dynamic/private";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+// if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 const client = postgres(env.DATABASE_URL);
 
