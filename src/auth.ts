c9537d7d698971066a2b44a61cbc7e2c4d9e9af5
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-02 11:42:10 +0900

    auth kind of working

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-import Auth0 from "@auth/sveltekit/providers/auth0";
-
+// import Auth0 from "@auth/sveltekit/providers/auth0";
+import Keycloak from "@auth/sveltekit/providers/keycloak";
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
-    Auth0,
+    Keycloak,
     // GitHub,
   ],
 });
