8b23f350b416f7e0e391777c054aaeed91c6351b
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-02 14:43:53 +0900

    auth0

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-// import Auth0 from "@auth/sveltekit/providers/auth0";
+import Auth0 from "@auth/sveltekit/providers/auth0";
 import Keycloak from "@auth/sveltekit/providers/keycloak";
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
-    Keycloak,
+    // Keycloak,
+    Auth0,
     // GitHub,
   ],
 });
