9150ca1e448d351f9ae2578c71164caf0b8eebfe
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-08 09:48:18 +0900

    minor sesssion checks

--- src/routes/texts/[id]/+page.server.ts
+++ src/routes/texts/[id]/+page.server.ts
@@ -5,7 +5,11 @@ import { eq } from "drizzle-orm";
 import type { PageServerLoad } from "./$types";
 import { getTextAndTokens } from "$lib/server/texts.js";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
+  const session = await locals.auth();
+  // TODO: actual error handling
+  if (!session?.user?.name) throw new Error("Unauthorized");
+  const user_id = session.user.name;
   const id = Number(params.id);
   const result = await getTextAndTokens(id);
 
--- src/routes/tokens/[id]/+page.server.ts
+++ src/routes/tokens/[id]/+page.server.ts
@@ -1,7 +1,12 @@
 import type { PageServerLoad } from "./$types";
 import { readToken } from "$lib/server/tokens";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
+  const session = await locals.auth();
+  // TODO: actual error handling
+  if (!session?.user?.name) throw new Error("Unauthorized");
+  const user_id = session.user.name;
+
   const id = Number(params.id);
   const token = await readToken(id);
   return token;
