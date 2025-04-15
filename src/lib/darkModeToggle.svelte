26eee31ff8b3470ff4b298ac89ae6cb218c040b2
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-16 08:44:15 +0900

    dark mode toggle

---
+++ src/lib/darkModeToggle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  function loadThemeFromLocalSorage() {
+    const html = document.querySelector("html");
+    if (!html) return;
+    const theme = localStorage.getItem("theme");
+    html.setAttribute("data-theme", theme || "light");
+  }
+
+  function changeTheme() {
+    const html = document.querySelector("html");
+    if (!html) return;
+    const currentTheme = html.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    html.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
+
+  loadThemeFromLocalSorage();
+</script>
+
+<button onclick={changeTheme} class="cursor-pointer text-2xl">
+  🌓︎
+  <!-- <img src="/dark-theme.svg" class="w-7" /> -->
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import DarkModeToggle from "$lib/darkModeToggle.svelte";
   import "../app.css";
 
   let { children } = $props();
@@ -10,9 +11,11 @@
   <title>Tokun</title>
 </svelte:head>
 
-<!-- <header class="p-2 border-b-1">
+<header class="p-2 border-b-1 flex justify-between">
   <h1 class="text-3xl">Tokun</h1>
-</header> -->
+  <DarkModeToggle />
+</header>
+
 <main class="max-w-2xl mx-auto p-2">
   {@render children()}
 </main>
