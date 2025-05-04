c350a62b2d80ffbc47638c0c2cb32bd9dce8e9dc
Maxime Moreillon <m.moreillon@gmail.com>
2025-05-05 07:02:30 +0900

    drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,48 +11,59 @@
   <title>Tokun</title>
 </svelte:head>
 
-<header class="navbar px-4">
-  <div class="navbar-start flex gap-6">
-    <div class="dropdown lg:hidden">
-      <div class="btn btn-ghost btn-circle" tabindex={0} role="button">
-        <Menu />
+<div class="drawer">
+  <input id="drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <header class="navbar px-4 w-full">
+      <div class="navbar-start flex gap-6">
+        <div class="dropdown lg:hidden">
+          <label
+            for="drawer"
+            aria-label="open sidebar"
+            class="btn btn-square btn-ghost"
+          >
+            <Menu />
+          </label>
+        </div>
+
+        <h1 class="text-3xl">Tokun</h1>
+
+        <nav class="hidden lg:flex tabs tabs-border flex-nowrap">
+          <a
+            role="tab"
+            class="tab"
+            class:tab-active={page.url.pathname.startsWith("/texts")}
+            href="/texts">Texts</a
+          >
+          <a
+            role="tab"
+            class="tab"
+            class:tab-active={page.url.pathname.startsWith("/tokens")}
+            href="/tokens">Tokens</a
+          >
+        </nav>
       </div>
-      <ul
-        class="menu dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-        tabindex={0}
-      >
-        <li>
-          <a href="/texts">Texts</a>
-        </li>
-        <li>
-          <a href="/tokens">Tokens</a>
-        </li>
-      </ul>
-    </div>
+      <div class="navbar-end flex gap-4">
+        <DarkModeToggle />
+        <LogoutButton />
+      </div>
+    </header>
 
-    <h1 class="text-3xl">Tokun</h1>
-
-    <nav role="tablist" class="hidden lg:flex tabs tabs-border flex-nowrap">
-      <a
-        role="tab"
-        class="tab"
-        class:tab-active={page.url.pathname.startsWith("/texts")}
-        href="/texts">Texts</a
-      >
-      <a
-        role="tab"
-        class="tab"
-        class:tab-active={page.url.pathname.startsWith("/tokens")}
-        href="/tokens">Tokens</a
-      >
-    </nav>
+    <main class="max-w-4xl mx-auto p-2 w-full">
+      {@render children()}
+    </main>
   </div>
-  <div class="navbar-end flex gap-4">
-    <DarkModeToggle />
-    <LogoutButton />
-  </div>
-</header>
 
-<main class="max-w-4xl mx-auto p-2">
-  {@render children()}
-</main>
+  <div class="drawer-side">
+    <label for="drawer" aria-label="close sidebar" class="drawer-overlay"
+    ></label>
+    <ul class="menu bg-base-200 text-base-content min-h-full w-60 p-4">
+      <li>
+        <a href="/texts">Texts</a>
+      </li>
+      <li>
+        <a href="/tokens">Tokens</a>
+      </li>
+    </ul>
+  </div>
+</div>
